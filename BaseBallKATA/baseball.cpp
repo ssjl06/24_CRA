43ce8177ae01f75b9c1f182e3d98a1b4a8c1b065
ssjl06.lee <ssjl06.lee@samsung.com>
2024-05-17 14:35:04 +0900

    refactor exception throwing code

--- BaseBallKATA/baseball.cpp
+++ BaseBallKATA/baseball.cpp
@@ -3,19 +3,28 @@
 class Baseball {
 public:
 	void guess(const std::string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+	}
+private:
+	void assertIllegalArgument(const std::string& guessNumber) {
 		if (guessNumber.length() != 3) {
 			throw std::length_error("Must be three letters");
 		}
 		for (char ch : guessNumber) {
-			if (ch < '0' || '9' < ch) {
-				throw std::invalid_argument("Must be a number");
-			}
+			if ('0' <= ch && ch <= '9') continue; 
+			throw std::invalid_argument("Must be a number");
 		}
+		if (isDuplicatedNumber(guessNumber)) {
+			throw std::invalid_argument("Must not have the same number.");
+		}
+	}
+	bool isDuplicatedNumber(const std::string& guessNumber) {
 		if (guessNumber[0] == guessNumber[1]
 			|| guessNumber[0] == guessNumber[2]
 			|| guessNumber[1] == guessNumber[2]
 			) {
-			throw std::invalid_argument("Must not have the same number.");
+			return true;
 		}
+		return false;
 	}
 };
