f8f9fab9f434422b045aaaaf8692d89cdb0025aa
ssjl06.lee <ssjl06.lee@samsung.com>
2024-05-17 10:53:47 +0900

    refactor Game

--- BallingGameTAKA/Game.cpp
+++ BallingGameTAKA/Game.cpp
@@ -1,27 +1,36 @@
 class Game
 {
 public:
-	Game() : score(0), pre(0), second_pre(0), throwing(0), frame(0) {}
+	Game() 
+		: score(0)
+		, pre_score(0)
+		, second_pre_score(0)
+		, throwing(0)
+		, frame(0) 
+	{
+	}
+
 	void Roll(int n) {
 		if (frame < 10) {
 			score += n;
 		}
-		if(pre==10 || second_pre==10){
-			if (pre == 10 && frame < 11) {
+		if(pre_score==10 || second_pre_score==10){
+			if (pre_score == 10 && frame < 11) {
 				score += n;
 			}
-			if (second_pre == 10 && frame < 12) {
+			if (second_pre_score == 10 && frame < 12) {
 				score += n;
 			}
 		}
-		else if (pre + second_pre == 10) {
+		else if (pre_score + second_pre_score == 10) {
 			score += n;
 		}
-		second_pre = pre;
-		pre = n;
+		second_pre_score = pre_score;
+		pre_score = n;
 		updateFrame(n);
 	}
-	int Score() {
+
+	int Score() const {
 		return score;
 	}
 
@@ -31,19 +40,18 @@ private:
 			if (n == 10) {
 				throwing += 2;
 				++frame;
+				return;
 			}
-			else {
-				throwing++;
-			}
+			throwing++;
+			return;
 		}
-		else {
-			++throwing;
-			++frame;
-		}
+		++throwing;
+		++frame;
 	}
+
 	int score;
-	int pre;
-	int second_pre;
+	int pre_score;
+	int second_pre_score;
 	int throwing;
 	int frame;
 };
