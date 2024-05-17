157dc5752aaf1da7806870e8a1a360d3f48d32fb
ssjl06.lee <ssjl06.lee@samsung.com>
2024-05-17 16:21:16 +0900

    refactor some

--- SimilarityCheckKATA/SimilarityCheck.cpp
+++ SimilarityCheckKATA/SimilarityCheck.cpp
@@ -8,12 +8,13 @@ public:
 	int getPoint(const std::string& str1, const std::string& str2) {
 		return getLengthPoint(str1, str2) + getAlphaPoint(str1, str2);
 	}
+
 	int getLengthPoint(const std::string& str1, const std::string& str2) {
 		auto length1 = static_cast<int>(str1.size());
 		auto length2 = static_cast<int>(str2.size());
 		auto gap = std::abs(length1 - length2);
-		auto shorter_one = std::min(length1, length2);
-		return (1 - (static_cast<double>(gap)/static_cast<double>(shorter_one))) * 60;
+		auto shorter_length = std::min(length1, length2);
+		return (1 - (static_cast<double>(gap)/static_cast<double>(shorter_length))) * 60;
 	}
 
 	int getAlphaPoint(const std::string& str1, const std::string& str2) {
@@ -30,11 +31,12 @@ public:
 			str2_set.insert(ch);
 		}
 		int same_cnt = 0;
+		int total_cnt = total_set.size();
 		for (auto ch : total_set) {
 			if (str1_set.count(ch) && str2_set.count(ch)) {
 				++same_cnt;
 			}
 		}
-		return (same_cnt/static_cast<double>(total_set.size())) * 40;
+		return (static_cast<double>(same_cnt)/total_cnt) * 40;
 	}
 };
