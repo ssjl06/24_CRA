d2fa7c9b8365cfca7413a65d3e1fb13b39b8f869
ssjl06.lee <ssjl06.lee@samsung.com>
2024-05-17 15:52:34 +0900

    implementation that passes 2nd test

--- SimilarityCheckKATA/SimilarityCheck.cpp
+++ SimilarityCheckKATA/SimilarityCheck.cpp
@@ -1,10 +1,14 @@
 #include <string>
+#include <cmath>
 
 class SimilarityChecker
 {
 public:
 	int getLengthPoint(const std::string& str1, const std::string& str2) {
-		return 60;
+		auto length1 = static_cast<int>(str1.size());
+		auto length2 = static_cast<int>(str2.size());
+		auto gap = std::abs(length1 - length2);
+		auto shorter_one = std::min(length1, length2);
+		return (1 - (gap/shorter_one)) * 60;
 	}
-
 };
--- SimilarityCheckKATATest/SimilarityCheckTest.cpp
+++ SimilarityCheckKATATest/SimilarityCheckTest.cpp
@@ -5,3 +5,8 @@ TEST(SimilarityCheckTest, lengthCheck) {
 	SimilarityChecker checker;
 	EXPECT_EQ(60, checker.getLengthPoint("ASD", "DSA"));
 }
+
+TEST(SimilarityCheckTest, lengthCheck2) {
+	SimilarityChecker checker;
+	EXPECT_EQ(0, checker.getLengthPoint("A", "BB"));
+}
